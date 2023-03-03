cb4abe7b5850ffc943497596e4eae3f33594de69
qixm <qixm@burnish.cn>
2023-03-03 16:23:45 +0800

    feat: 添加流程说明

--- README.md
+++ README.md
@@ -1,56 +1,32 @@
 # Vue Web Loader 2
 
-## 项目说明
-
-Vue Web Loader 2 可以在不使用前端工具(如npm,webpack等)的情况下，快速构建基于 Vue 的单页面应用，具有以下优点：依赖小、支持vue单页面组件、支持动态加载组件。
-
-## 使用方式
-
-1、在html页面上引入Vue和vue-web-loader-2.js，并加上Vue程序的挂载点
-```
-<script src="js/vue@2.7.14.min.js"></script>
-<script src="js/vue-web-loader-2.js"></script>
-
-<!-- 挂载点 -->
-<div id="app"></div>
-```
-
-2、使用下面代码加载入口文件
-```
-<script type="text/javascript">
-    VueWebLoader({
-        version: Date.now(), // 加载文件时指定的版本号，通过Date.now()来避免浏览器缓存问题
-        entry: 'view/vue2.js', // 入口文件
-    });
-</script>
-```
-
-3、入口文件要按Vue的要求去启动
-```
-// vue2.js
-import Vue from 'vue'
-import app from './app.vue'
-
-window.vueapp = new Vue({
-    el: '#app',
-    data: {},
-    render: function(h) {
-        return h(app);
-    }
-});
-```
-
-
-```
-// vue3.js
-import Vue from 'vue'
-import app from './app.vue'
-
-window.vueapp = Vue.createApp(app);
-window.vueapp.mount('#app');
-```
-
-4、特别注意：
-
-- import语句会先判断是否全局变量，如果是全局变量会直接使用，不会去加载后面对应文件；
-- import语句如果临时想要取消，必须用多行注释/**/进行注释，单行注释依然会被解析出来；
+## 具体流程如下：
+通过`VueWebLoader`对象加载入口文件，开始Web应用的远程加载。
+1. 解析传入的version选项，在后续远程加载时会添加为v参数
+2. 解析得到入口文件路径entry，使用`fetch`远程加载入口文件`requestGet`
+3. 获取入口文件的文本内容，并开启解析回调
+4. 解析网页的根路径为`base_url`
+5. 解析`parseImport`入口文件文本，修改所有的导入语句：
+   1. 删除搜有注释
+   2. 替换代码（`import xx from yy.js/vue`语句注释，并添加`var xx=windows.VueWebLoader.registered_component[.js/vue的http链接];`）
+   3. `import xx`语句注释，并添加`var xx=windows.xx`
+   4. 返回被修改的源代码，所有的`.js/vue`的http链接
+6. 加载`importComponents`所有的`.js/vue`的http链接
+   1. 加载完成所有的http链接，`.js`使用`requestJs`解析脚本内容，`.vue`使用`requestVue`解析脚本内容
+   2. 执行回调：执行`5`返回的源码内容
+7. `requestVue`解析Vue文件流程如下：
+   1. 解析出script、template、style文本内容，template和style通过`document.createElement`方式获取
+   2. 使用步骤`5`解析script内容；
+   3. `importComponents`加载加载完所有的`.js/vue`的http链接——递归加载
+   4. 使用`registerVueSFC`注册vue组件
+      1. style添加到head中；
+      2. template添加为vue options的template字段
+      3. 通过`Vue.component`注册组件并返回
+   5. 检查所属父组件的全部子组件是否已加载完成
+   6. 执行回调（一般都没有）
+8. `requestJs`解析js文件流程如下：
+   1. 使用步骤`5`解析脚本内容内容；
+   2. 使用`importComponents`加载完所有的`.js/vue`的http链接，在回调中执行脚本内容
+   3. 检查所属父组件的全部子组件是否已加载完成
+9.  
+10. 
--- js/vue-web-loader-2.js
+++ js/vue-web-loader-2.js
@@ -3,17 +3,17 @@
     // vue大版本号
     var vue_version = parseInt(Vue.version.split('.')[0]);
     var vue_app = vue_version == 3 ? Vue.createApp() : null;
-    // 应用版本号
+    // 应用版本号：后面远程加载入口文件时，会在路径上添加v版本号
     var request_version = '';
     // 所有请求的base地址
     var base_url = '';
-    // 全局组件关系表
+    // 全局组件关系表：值表示是否被加载完成
     var components_relation = {};
     // 全局组件回调表
     var components_callback = {};
     // 全局加载中组件表
     var requesting_component = {};
-
+  
     if (!String.prototype.endsWith) {
         String.prototype.endsWith = function(search, this_len) {
             if (this_len === undefined || this_len > this.length) {
@@ -22,31 +22,31 @@
             return this.substring(this_len - search.length, this_len) === search;
         };
     }
-
+  
     function evalJs(url, script) {
-
-        // 'console.log("this is ' + url + '");\n\n' + 
-
+  
+        // 'console.log("this is ' + url + '");\n\n' +
+  
         script = script + '\n\n' + '//# sourceURL=' + url;
-
+  
         return eval(script);
     }
-
-    // 加载远程文件
+  
+    // 加载远程文件：返回url路径和文本内容
     function requestGet(url, callback) {
-
+  
         // 判断是否要加版本号
         if (request_version != '') {
-
+  
             // 判断地址中是否已经有其他参数
             str = url.indexOf('?') > 0 ? '&v=' : '?v=';
-
+  
             url = url + str + request_version;
         }
-
+  
         // 请求文件
         var full_url = '';
-
+  
         return fetch(url).then(function(response) {
             var url_obj = new URL(response.url);
             full_url = url_obj.href.replace(url_obj.search, '');
@@ -59,7 +59,8 @@
             throw value;
         });
     }
-
+  
+    // 删除所有的多行注释
     function removeComments(script) {
         // 匹配所有多行注释
         var regexp = new RegExp(/\/\*[\s\S]*?\*\//);
@@ -67,85 +68,95 @@
         while ((match = regexp.exec(script)) != null) {
             script = script.replace(match[0], '');
         }
-
+  
         // // 匹配单行注释，仅匹配以//开头的行
         // var regexp = new RegExp(/[^\\]\/\/.*/);
         // // 将单行注释删除
         // while ((match = regexp.exec(script)) != null) {
         //     script = script.replace(match[0], '');
         // }
-
+  
         return script;
     }
-
+  
     function parseAbsoluteUrl(url, parent_url) {
         var url_obj = new URL(url, parent_url)
         return url_obj.href;
     }
-
+  
+    /**
+     * 1、删除搜有注释
+     * 2、替换代码（import xx from yy.js/vue语句注释，并添加var xx=windows.VueWebLoader.registered_component[.js/vue的http链接];）
+     * 3、import xx语句注释，并添加var xx=windows.xx
+     * 4、返回剩余的源代码，所有的.js/vue的http链接
+     * @param {项目的URL跟路径} parent_url 如：http://192.168.3.252:3000/
+     * @param {入口文件文本内容} script
+     * @returns
+     */
     function parseImport(parent_url, script) {
-
+  
         var list = [];
-
+  
+        // 删除所有的多行注释
         script = removeComments(script);
-
+  
         if (!script) {
             return { script: script, list: list };
         }
-
+  
         // 深度复制字符串
         var script_copy = (' ' + script).slice(1);
-
+  
+        // 匹配出所有的import语句
         var regexp = RegExp(/import([\s\S].+)from([\s\S].+["'])/, 'g');
-
         var matches = script.matchAll(regexp);
-
+  
         for (var match of matches) {
-
-            // 匹配到的import代码
+  
+            // 匹配到的import语句
             var m_content = match[0];
-            // 取变量名称
+            // 取变量名称：导入变量名
             var v_name = match[1].trim();
-            // 取文件名称
+            // 取文件名称：导入文件名
             var f_name = match[2].replaceAll(/["'\s]/ig, '');
-            // 前置代码
+            // 前置代码（import语句 改为var xx=windows.）
             var prev_code = '\n/*\n' + m_content + '\n*/\nvar ' + v_name + ' = window.';
-
+  
             // 判断是否以.js或.vue结尾
             if (f_name.endsWith('.js') || f_name.endsWith('.vue')) {
-                // 获取引用文件的绝对地址
+                // 获取引用文件的绝对地址：生成http的文件路径
                 var absolute_url = parseAbsoluteUrl(f_name, parent_url);
-                // 替换代码
+                // 替换代码（import语句注释，并添加var xx=windows.VueWebLoader.registered_component[.js/vue的http链接];）
                 script_copy = script_copy.replace(m_content, prev_code + 'VueWebLoader.registered_component["' + absolute_url + '"];\n');
                 // 将地址添加到请求列表里
                 list.push(absolute_url);
                 continue;
             }
-
-            // 不是以.js或.vue结尾时，直接替换成windows对象的引用
+  
+            // 不是以.js或.vue结尾时，直接替换成windows对象的引用：import xx语句注释，并添加var xx=windows.xx
             script_copy = script_copy.replace(m_content, prev_code + v_name + ';\n');
         }
-
+  
         return { script: script_copy, list: list };
     }
-
+  
     function checkParentImportProgress(url) {
-
+  
         for (var parent in components_relation) {
             // 用以标记当前父组件是否需要再遍历检查一次
             var parent_list = components_relation[parent];
-
+  
             var reloop = false;
-
+  
             for (var child in parent_list) {
-
+  
                 if (child == url) {
                     reloop = true;
                     parent_list[url] = true;
                     components_relation[parent][url] = true;
                 }
             }
-
+  
             if (reloop == true) {
                 var is_finish = true;
                 for (var child in parent_list) {
@@ -153,15 +164,15 @@
                         is_finish = false;
                     }
                 }
-
+  
                 if (is_finish == true) {
                     // 该组件的所有子组件已加载完成
                     var finish_callback = components_callback[parent];
-
+  
                     if (finish_callback) {
                         finish_callback();
                     }
-
+  
                     // 从关系表和回调表中，删除该组件
                     delete components_relation[parent];
                     delete components_callback[parent];
@@ -169,29 +180,34 @@
             }
         }
     }
-
+  
+    /**
+     * 从vue文件中解析出对应的script、template、style
+     * @param {vue文件内容} content
+     * @returns
+     */
     function parseVueSFC(content) {
-
+  
         var regexp = new RegExp(/<script[\s\S]*?>([\s\S]*)<\/script>/, 'g');
-
+  
         var match = regexp.exec(content);
-
+  
         var script = match ? match[1] : '';
-
+  
         var style = '';
-
+  
         var template = '';
-
+  
         // 创建一个div节点
         var element = document.createElement('div');
-
+  
         // 将vue组件的内容添加到节点上
         element.innerHTML = content;
-
+  
         for (var i = 0; i < element.children.length; i++) {
-
+  
             var child = element.children[i];
-
+  
             if (child.tagName == 'TEMPLATE') {
                 template = child.innerHTML;
                 template = template.replace('inside-script', 'script')
@@ -202,196 +218,209 @@
             //     script = child.innerHTML;
             // }
         }
-
+  
         return {
             style: style,
             script: script,
             template: template,
         }
     }
-
+  
     function parseUrlToName(url) {
         // 去掉最后的文件名，再去掉base_url，再替换/为_，最后加上name
         return url.substring(0, url.lastIndexOf('.')).replace(base_url, '').replace(/\//g, '_');
     }
-
+  
     function appendStyle(name, style) {
-
+  
         var el = document.createElement('style');
-
+  
         el.id = name;
         el.type = 'text/css';
         el.innerHTML = style;
-
+  
         var head = document.querySelector('head');
         head.appendChild(el);
     }
-
+  
+    /**
+     * 1、将style添加到html的head中
+     * 2、
+     * @param {*} url
+     * @param {*} template
+     * @param {*} script
+     * @param {*} style
+     * @returns
+     */
     function registerVueSFC(url, template, script, style) {
-
+  
         var name = parseUrlToName(url);
-
+  
         // 添加css到页面中
         if (style) {
             appendStyle(name, style);
         }
-
+  
         // 将模版保存到全局变量中
         VueWebLoader.component_template[name] = template;
-
+  
         // 分析script
         var regexp = new RegExp(/([\S\s]*)export\s+default\s+{([\S\s]*})/, 'g');
-
+  
         var match = regexp.exec(script);
-
+  
         var js = (match[1] ? match[1] : '') + '\n (function(){ return {\n    template: VueWebLoader.component_template["' + name + '"],' + match[2] + '})();';
-
-        // 执行script
+  
+        // 执行script：将script中的export改为自执行函数，返回一个vue组件的options，渲染方式改为template
         var setting = evalJs(url, js);
-
+  
         var component = null;
-
+  
         if (vue_version == '3') {
             vue_app.component(name, setting);
             component = vue_app.component(name);
         } else {
             component = Vue.component(name, setting);
         }
-
+  
         delete VueWebLoader.component_template[name];
-
+  
         return component;
     }
-
+  
     function requestVue(vue_url, callback) {
-
+  
         // 检查加载状态
         if (requesting_component[vue_url] == true) {
             return true;
         }
-
+  
         // 设置加载状态
         requesting_component[vue_url] = true;
-
+  
         // 通过异步请求该文件
         requestGet(vue_url, function(url, content) {
-
-            // 分析vue模版内容
+  
+            // 分析vue模版内容：script、template、style
             var result = parseVueSFC(content);
-
+  
             var style = result.style;
             var script = result.script;
             var template = result.template;
-
+  
             var result2 = parseImport(url, script);
             var script = result2.script;
             var list = result2.list;
-
+  
             importComponents(url, list, function() {
-
+  
                 // 注册vue组件
                 VueWebLoader.registered_component[url] = registerVueSFC(url, template, script, style);
-
+  
                 // 检查所属父组件的全部子组件是否已加载完成
                 checkParentImportProgress(url);
-
+  
                 // 如果提提供了回调函数
                 if (callback) {
                     // 则将执行js代码后的结果返回给回调函数
                     callback(VueWebLoader.registered_component[url]);
                 }
-
+  
             });
-
+  
         });
     }
-
+  
+    // 加载js文件
     function requestJs(js_url, callback) {
-
-        // 检查加载状态
+  
+        // 检查加载状态：已处于加载状态，则直接返回
         if (requesting_component[js_url] == true) {
             return true;
         }
-
+  
         // 设置加载状态
         requesting_component[js_url] = true;
-
+  
         // 通过异步请求该文件
         requestGet(js_url, function(url, content) {
-
+  
             // 分析脚本中是否包含import语句
             var result = parseImport(url, content);
             var script = result.script;
             var list = result.list;
-
+  
+            // 导入组件
             importComponents(url, list, function() {
-
+  
                 script = '\n window.VueWebLoader.registered_component["' + url + '"] = (function(){\n' +
                     script.replace('export default ', 'return ') + '\n})();';
-
+  
                 // 执行JS文件
                 evalJs(url, script);
-
+  
                 // 检查所属父组件的全部子组件是否已加载完成
                 checkParentImportProgress(url);
-
+  
                 // 如果提提供了回调函数
                 if (callback) {
                     // 则将执行js代码后的结果返回给回调函数
                     callback(VueWebLoader.registered_component[url]);
                 }
-
+  
             });
         });
     }
-
-
+  
+  
     function importComponents(parent_url, list, callback) {
+        // 未被导入的初始化
         if (!components_relation[parent_url]) {
             components_relation[parent_url] = {};
             components_callback[parent_url] = callback;
         }
-
-        // 判断是否存在下级组件
+  
+        // 判断是否存在下级组件：如果不存在待加载的远程脚本文件则直接执行返回
         if (list.length <= 0) {
             // 当不存在下级组件时，直接执行回调函数
             return callback();
         }
-
+  
         var is_finish = true;
         var child_url = '';
-
+  
         var len = list.length;
-
+  
+        // 加载所有远程链接
         for (var i = 0; i < len; i++) {
             var url = list[i];
-
+  
             // 判断该组件是否已加载
             if (VueWebLoader.registered_component[url]) {
-
+  
                 // 修改组件状态
                 components_relation[parent_url][url] = true;
-
+  
                 // 记录组件地址，方便下面使用
                 child_url = url;
             } else {
-
+  
                 is_finish = false;
                 // 修改组件状态
                 components_relation[parent_url][url] = false;
-
+  
                 // 判断该组件是否正在加载
                 if (requesting_component[url]) {
                     // 正在加载的话，直接跳过
                     continue;
                 }
-
+  
                 // 如果是vue组件
                 if (url.endsWith('.vue')) {
                     requestVue(url);
                     continue;
                 }
-
+  
                 // 如果是js文件
                 if (url.endsWith('.js')) {
                     requestJs(url);
@@ -399,46 +428,50 @@
                 }
             }
         }
-
+  
         if (is_finish == true) {
             checkParentImportProgress(child_url)
         }
     }
-
+  
     var VueWebLoader = function(setting) {
-
+  
         if (setting.version) {
             request_version = setting.version;
         }
-
+  
         var start_time = Date.now();
-
+  
+        // 项目的入口文件路径
         var entry = setting.entry;
-
+  
         console.log('开始加载Vue项目入口JS文件：' + entry);
-
-        requestGet(entry, function(url, content) {
-
-            // 计算base_url
+  
+  
+        requestGet(entry, function(url, content) { // url：入口文件的url路径，content：入口文件的文本内容
+  
+            // 计算base_url，如http://192.168.3.252:3000/
             base_url = url.substr(0, url.indexOf(entry.replaceAll('../', '').replaceAll('./', '')));
-
+  
+            // 将所有的import注释并修改导入语句
             var result = parseImport(url, content);
-
+  
+            // 修改后的脚本内容
             var script = result.script;
+            // 所有待加载的js/vue文件超链接集合
             var list = result.list;
-
+  
             // 分析加载组件
             importComponents(url, list, function() {
-
-                // 执行入口文件
+                // 执行入口文件：子组件导入完成后执行
                 evalJs(url, script);
                 // 输出执行耗时信息
                 console.log('加载Vue项目入口JS文件完成，耗时:' + (Date.now() - start_time) / 1000 + ' 秒');
-
+  
             });
         });
     }
-
+  
     // 填充对象
     window.VueWebLoader = Object.assign(VueWebLoader, {
         component_template: {},
@@ -449,12 +482,13 @@
             if (url.endsWith('.vue')) {
                 requestVue(url, callback);
             }
-
+  
             // 如果是js文件
             if (url.endsWith('.js')) {
                 requestJs(url, callback);
             }
         }
     });
-
-})(window, document, Vue);
+  
+  })(window, document, Vue);
+  
